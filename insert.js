052e74225a4572e29fef9b9a8e4dd1a81fc6c7b4
Christian Autermann <c.autermann@52north.org>
2019-09-30 11:49:02 +0200

    added privacy statements insertion

--- insert.js
+++ insert.js
@@ -1,5 +1,6 @@
 let date = new Date().toISOString().substring(0,10)
 
+db.termsOfUse.remove({issuedDate: date});
 db.termsOfUse.insert({
   _id: new ObjectId(),
   created: date + "T12:00:00Z",
@@ -7,5 +8,16 @@ db.termsOfUse.insert({
   contents: cat("TOU_EN.html").replace("\n", "\\n").replace(" +", " "),
   translations: {
     de: cat("TOU_DE.html").replace("\n", "\\n").replace(" +", " ")
+  }
+});
+
+db.privacyStatements.remove({issuedDate: date});
+db.privacyStatements.insert({
+  _id: new ObjectId(),
+  created: date + "T12:00:00Z",
+  issuedDate: date,
+  contents: cat("dataPriv_EN.html").replace("\n", "\\n").replace(" +", " "),
+  translations: {
+    de: cat("dataPriv_DE.html").replace("\n", "\\n").replace(" +", " ")
   }
 });
