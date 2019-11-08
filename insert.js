7232d3a3aa762beb02e007829ee62ca1358f2954
Christian Autermann <c.autermann@52north.org>
2019-11-08 13:51:55 +0100

    Update insert.js

--- insert.js
+++ insert.js
@@ -1,4 +1,6 @@
-let date = new Date().toISOString().substring(0, 10);
+//let date = new Date().toISOString().substring(0, 10);
+
+let date = '2019-07-14';
 
 db.termsOfUse.remove({issuedDate: date});
 db.termsOfUse.insert({
