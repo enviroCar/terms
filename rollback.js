d7f6eb560726229ffe11eadcef0d4c25f6ae884d
Matthes Rieke <m.rieke@52north.org>
2023-03-01 10:10:50 +0100

    adjust release date

--- rollback.js
+++ rollback.js
@@ -1,7 +1,7 @@
 //let date = new Date().toISOString().substring(0, 10);
 
 //let date = '2019-07-14';
-let date = '2022-05-31';
+let date = '2023-03-01';
 
 db.termsOfUse.remove({issuedDate: date});
 db.privacyStatements.remove({issuedDate: date});
